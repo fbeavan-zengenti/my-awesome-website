edb70bb5d01873e88cf258e7cfdfb705bedbea0b
Felicity Beavan <f.beavan@zengenti.com>
2024-04-08 16:21:41 +0100

    update background colour

--- src/app/theme/global/globalStyles.js
+++ src/app/theme/global/globalStyles.js
@@ -24,7 +24,7 @@ const GlobalStyle = createGlobalStyle`
     font-weight: ${fontWeight.regular};
     line-height: ${fontBase.lineHeight};
     color: ${colors.neutral_charcoal};
-    background-color: #fff;
+    background-color: #ddf8ce;
   }
   h1,h2,h3,h4,h5,h6 {
     margin:0;
